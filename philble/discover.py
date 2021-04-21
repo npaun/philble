abb4de510646e4833c05978d60a08dd89fb5101e
Daniel Wagner <wagner.daniel.t@gmail.com>
2021-04-20 21:00:01 -0400

    struggle on when list of found devices doesn't match expected

--- philble/discover.py
+++ philble/discover.py
@@ -4,6 +4,11 @@ import os
 unbuffered = os.fdopen(sys.stdout.fileno(), 'wb', 0)
 MAX_TICKS = 40
 
+HUE_DEVICE_NAMES = frozenset([
+    'Hue Lamp',
+    'Hue ambiance lamp',
+])
+
 def discover(ble, config):
     unbuffered.write(b'Discovering')
     ble.clear_cached_data()
@@ -13,22 +18,30 @@ def discover(ble, config):
     adapter.power_on()
     adapter.start_scan()
 
-    found = False
     for _ in range(MAX_TICKS):
         unbuffered.write(b'.')
         time.sleep(.5)
-        lights = [dev for dev in ble.find_devices() if dev.name == 'Hue Lamp']
+        lights = [dev for dev in ble.find_devices() if dev.name in HUE_DEVICE_NAMES]
         if len(lights) == len(config):
-            found = True
             break
 
-    if found:
-        print('ok')
-        for light in lights:
-            light.connect()
-    else:
-        print('fail')
+    if len(lights) == 0:
         raise SystemExit
 
+    new_lights = 0
+    names = dict()
+
+    print('found %d lights:' % len(lights))
+    for light in lights:
+        if light.id in config:
+            name = config[light.id]
+        else:
+            new_lights += 1
+            name = "new_%d" % new_lights
+
+        names[name] = light
+        print("%s (%s %s)" % (name, light.name, light.id))
+        light.connect()
+
     time.sleep(.5)
-    return {config[light.id]:light for light in lights}
+    return names
